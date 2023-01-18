2c60f8becbf01b58cd8a38e1face8fe34640d6fd
Pavel <whatisclamp@gmail.com>
2023-01-18 12:44:45 +0200

    feat: add table company and columns for company. install nestjs/sequelize

---
+++ server/src/companies/models/company.model.ts
@@ -0,0 +1,17 @@
+import { Table, Model, Column } from 'sequelize-typescript';
+
+@Table
+export class Company extends Model {
+  @Column
+  name: string;
+  @Column
+  adress: string;
+  @Column
+  service: string;
+  @Column
+  numOfEmployees: string;
+  @Column
+  description: string;
+  @Column
+  type: string;
+}
