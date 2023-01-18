38b1f42ca065fdf16667d1dbf6f9dd8b0f3397e8
Pavel <whatisclamp@gmail.com>
2023-01-18 13:02:49 +0200

    feat: add functions for find one compy, find all companies, change company and remove company

---
+++ server/src/companies/company.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { ChangeCompany } from './dto/change-company.dto';
+import { CreateCompany } from './dto/create-company.dto';
+import { Company } from './models/company.model';
+
+@Injectable()
+export class CompanyService {
+  constructor(
+    @InjectModel(Company)
+    private companyModel: typeof Company,
+  ) {}
+
+  async findAll(): Promise<Company[]> {
+    return this.companyModel.findAll();
+  }
+
+  findOne(id: string): Promise<Company> {
+    return this.companyModel.findOne({
+      where: {
+        id,
+      },
+    });
+  }
+
+  create(createCompany: CreateCompany): Promise<Company> {
+    const company = new Company();
+
+    company.name = createCompany.name;
+    company.adress = createCompany.adress;
+    company.service = createCompany.service;
+    company.numOfEmployees = createCompany.numOfEmployees;
+    company.description = createCompany.description;
+    company.type = createCompany.type;
+
+    return company.save();
+  }
+
+  update(
+    id: string,
+    changeCompany: ChangeCompany,
+  ): Promise<[affectedCount: number, affectedRows: Company[]]> {
+    return this.companyModel.update(
+      { ...changeCompany },
+      {
+        where: {
+          id,
+        },
+        returning: true,
+      },
+    );
+  }
+
+  async remove(id: string): Promise<void> {
+    const company = await this.findOne(id);
+    await company.destroy();
+  }
+}
